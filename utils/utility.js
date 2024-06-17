ccfbe3577df63ffce2b80a327e57866ea211708c
tommasogiannoni <tommaso.giannoni@yahoo.it>
2024-06-17 13:43:15 +0200

    removed comment

--- utils/utility.js
+++ utils/utility.js
@@ -2,7 +2,7 @@ export function formatDate(isoDate) {
     const date = new Date(isoDate);
     
     const day = String(date.getUTCDate()).padStart(2, '0');
-    const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // I mesi vanno da 0 a 11, quindi aggiungiamo 1
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
     const year = date.getUTCFullYear();
     
     return `${month}/${day}/${year}`;
